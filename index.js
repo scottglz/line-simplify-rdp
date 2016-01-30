9039db58ae55512efb031451eecafcb54732b4de
scottglz <grapes97223@gmail.com>
2016-01-30 03:25:20 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var distanceToLineSegment2 = require('distance-to-line-segment').squaredWithPrecalc;
+
+function simplifyInternal(points, idx1, idx2, threshold, keepers) {
+   keepers[idx1] = keepers[idx2] = true;
+   if (idx2 <= idx1+1) 
+      return;
+   
+   
+   var point1 = points[idx1],
+       point2 = points[idx2],
+       x1 = point1.x,
+       y1 = point1.y,
+       dx = point2.x - x1,
+       dy = point2.y - y1,
+       lineLengthSquared = dx*dx + dy*dy;
+   
+   var minDist = Number.POSITIVE_INFINITY,
+       minDistIdx = -1;
+   for (var i=idx1+1; i < idx2; i++) {
+      var pointBetween = points[i];
+      var dist = distanceToLineSegment2(x1, y1, dx, dy, lineLengthSquared, pointBetween.x, pointBetween.y);
+      if (dist < minDist) {
+         minDist = dist;
+         minDistIdx = i;
+      }
+   }
+   
+   if (minDist <= threshold) 
+      return; 
+   
+   simplifyInternal(points, idx1, minDistIdx, threshold, keepers);
+   simplifyInternal(points, minDistIdx, idx2, threshold, keepers);
+   
+   
+}
+
+
+function simplify(points, threshold) {
+   var len = points.length;
+   if (len <= 2)
+      return points;
+   
+   var keepers = [];
+   simplifyInternal(points, 0, len-1, threshold, keepers);
+   
+   var out = [];
+   for (var i=0; i < len; i++) {
+      if (keepers[i])
+         out.push(points[i]);
+   }
+   return out;
+}
+
+module.exports = simplify;
---
+++ test.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var test = require('tape');
+var simplify = require('.');
+
+test("stairstep", function(t) {
+    t.plan(4);
+    var stairstep = [ { x:0, y: 0}];
+    for (var i=1; i < 4; i++) {
+        stairstep.push({
+            x: Math.floor((i)/2),
+            y: Math.floor((i+2)/2)
+        });
+        var simplified = simplify(stairstep, 1);
+        t.deepEquals(simplified, [stairstep[0],stairstep[i]], (i+1) + " points")
+    }
+    
+    for (var i=3; i < 1000; i++) {
+        stairstep.push({
+            x: Math.floor((i)/2),
+            y: Math.floor((i+2)/2)
+        });
+    }
+     t.deepEquals(simplify(stairstep, 1 ), [stairstep[0],stairstep[999]], "1000 points")
+});
+
+test("unsimplifiable", function(t) {
+    t.plan(1);
+    var points = [
+        { x: 15, y: 60 },
+        { x: 8, y: 45 },
+        { x: 19, y: 16 },
+        { x: 27, y: 42 }
+    ];
+    
+    t.deepEquals(simplify(points, 1), points);
+});
